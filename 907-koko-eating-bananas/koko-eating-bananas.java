aff1dce7b95a8070e8edd4570ea36aa5924ca737
A Mohamed yusuff <Yusuff.a968@gmail.com>
2026-08-26 11:43:56 +0530

    Time: 14 ms (38.98%) | Memory: 47.7 MB (74.57%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left=1,right=0;
+
+        for (int pile:piles){
+            right=Math.max(pile,right);
+        }
+
+        int ans=right;
+
+        while (left<=right){
+            int mid=left+(right-left)/2;
+            long time=0;
+
+            for (int pile:piles){
+                time+= pile/mid;
+                time+= pile%mid==0?0:1;
+            }
+
+            if (time<=h){
+                ans=Math.min(ans,mid);
+                right=mid-1;
+            } else {
+                left=mid+1;
+            }
+        }
+        return ans;
+    }
+}
