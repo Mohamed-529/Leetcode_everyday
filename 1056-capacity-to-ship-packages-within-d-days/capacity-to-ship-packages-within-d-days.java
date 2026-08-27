59d5f11b425f3bd3a665a0f1b198c7196618f936
A Mohamed yusuff <Yusuff.a968@gmail.com>
2026-08-27 14:22:23 +0530

    Time: 10 ms (82.44%) | Memory: 50.3 MB (38.01%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,32 +1,32 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        
         int left=0,right=0;
-        for (int num:weights){
-            left=Math.max(num,left);
-            right+=num;
-        }
-        int sum=right,ans=0;
 
-        while (left <= right){
+        for (int each:weights){
+            right+=each;
+            left=Math.max(each,left);
+        }
+        
+
+        while (left<=right){
             int mid=left+(right-left)/2;
-            
-            int isFit=0,count=1;
-            for(int num:weights){
-                isFit+=num;
-                if(isFit>mid){
-                    isFit=num;
-                    count++;
+
+            int wei=0,day=1;
+            for (int each:weights){
+                if(wei+each > mid){
+                    day++;
+                    wei=each;
+                }else {
+                    wei+=each;
                 }
             }
 
-            if (count<=days){
-                ans=mid;
+            if (day<=days){
                 right=mid-1;
             } else {
                 left=mid+1;
             }
         }
-        return ans;
+        return left;
     }
 }
