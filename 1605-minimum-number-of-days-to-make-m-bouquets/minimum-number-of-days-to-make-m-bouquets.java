60de56893bb8b956661254e7e8bc2f3d712e856f
A Mohamed yusuff <Yusuff.a968@gmail.com>
2026-08-30 10:28:19 +0530

    Time: 15 ms (97.55%) | Memory: 82.3 MB (92.05%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,36 +1,41 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-     int left=1,right=0;
 
-     for (int bloom:bloomDay){
-        right=Math.max(bloom,right);
-     }
-     int ans=-1;
+        if (bloomDay.length<(long)m*k){
+            return -1;
+        }
+        int left=1,right=0;
 
-     while (left<=right){
-        int mid=left+(right-left)/2;
-
-        int bouq=0,adj=0;
         for (int bloom:bloomDay){
-            if (bloom<=mid){
-                adj++;
-                if(adj==k){
-                    bouq++;
-                    adj=0;
+            right=Math.max(bloom,right);
+        }
+
+        int ans=right;
+
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            int bouquet=0,flowers=0;
+
+
+            for (int bloom:bloomDay){
+                if(bloom<=mid){
+                    flowers++;
+                    if(flowers==k){
+                        bouquet++;
+                        flowers=0;
+                    }
+                } else {
+                    flowers=0;
                 }
+            }
+
+            if (bouquet>=m){
+                ans=Math.min(ans,mid);
+                right=mid-1;
             } else {
-                adj=0;
+                left=mid+1;
             }
         }
-
-        if (bouq>=m){
-            ans=mid;
-            right=mid-1;
-        } else {
-            left=mid+1;
-        }
-     }
-
-     return ans;   
+        return ans;
     }
 }
