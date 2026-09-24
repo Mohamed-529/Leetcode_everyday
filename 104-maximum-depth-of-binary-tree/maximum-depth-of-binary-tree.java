68b5268cadd8473d57a5f46c71c0242299bc6cfd
A Mohamed yusuff <Yusuff.a968@gmail.com>
2026-09-24 15:19:23 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (66.54%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -19,6 +19,9 @@ class Solution {
             return 0;
         }
 
-        return 1 + Math.max(maxDepth(root.left),maxDepth(root.right));
+        int left=maxDepth(root.left);
+        int right=maxDepth(root.right);
+
+        return 1 + Math.max(left,right);
     }
 }
